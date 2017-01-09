17b5981158947a607d496738bd49873f866a4d9b
Chris Arriola <arriolac279@gmail.com>
2017-01-09 13:34:40 +0100

    Checking in cryptolib.

--- challenge1.py
+++ challenge1.py
@@ -1,7 +1,7 @@
-import lib
+import crypto_lib
 
 def main():
-    print lib.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+    print crypto_lib.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 
 if __name__ == '__main__':
     main()
---
+++ crypto_lib/__init__.py
@@ -0,0 +1,2 @@
+def hex_to_base64(s):
+    return s.decode("hex").encode("base64")
