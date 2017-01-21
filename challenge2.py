31dc5292765ca70775361c2d5bb1395a83657bb3
Chris Arriola <arriolac279@gmail.com>
2017-01-21 21:29:53 +0100

    Challenge 2.

---
+++ challenge2.py
@@ -0,0 +1,7 @@
+import crypto_lib
+
+def main():
+    print crypto_lib.fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+
+if __name__ == '__main__':
+    main()
--- crypto_lib/__init__.py
+++ crypto_lib/__init__.py
@@ -1,2 +1,7 @@
 def hex_to_base64(s):
     return s.decode("hex").encode("base64")
+
+def fixed_xor(s1, s2):
+    return ''.join("{0:x}".format(int(i, 16) ^ int(j, 16)) for i,j in zip(s1, s2))
+
+
